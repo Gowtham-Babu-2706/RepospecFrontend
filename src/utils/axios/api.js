77885fd7cdc6dc5a5cddb6cb0234d9ea2a4996b6
Gowtham-Babu-2706 <gowthambabu27062006@gmail.com>
2026-06-21 19:15:25 +0530

    After Adding the Message

--- src/utils/axios/api.js
+++ src/utils/axios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:8080/api/v1/repos",
+    baseURL:"https://repospecbackend-1.onrender.com/api/v1/repos",
     timeout:20000,
     headers:{
         "Content-Type":"application/json",
